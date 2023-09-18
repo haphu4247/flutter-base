932fb7099e999d38bd3ba783a3a315afec97273d
Phu <haphu4247@gmail.com>
2023-09-18 11:58:28 +0700

    - add firebase config.
    - android/ios/web/macos.

--- base_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
+++ base_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,7 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import firebase_analytics
 import firebase_core
 import firebase_crashlytics
 import firebase_messaging
@@ -16,7 +15,6 @@ import shared_preferences_foundation
 import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
