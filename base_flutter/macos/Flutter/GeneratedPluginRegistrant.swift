23750ac00cf07221afae6424a00103b4ecf03ea6
PhuHP <ldcc.phuha@gmail.com>
2024-07-24 14:09:04 +0700

    Update GeneratedPluginRegistrant.swift

--- base_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
+++ base_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -14,6 +14,7 @@ import geolocator_apple
 import package_info_plus
 import path_provider_foundation
 import share_plus
+import shared_preferences_foundation
 import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -26,5 +27,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
